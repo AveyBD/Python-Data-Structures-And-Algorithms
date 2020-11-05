d5444326c28b3b6e5ceb462954d01394e1cdf104
Debashish Das <talk@ddeb.me>
2020-11-05 19:42:59 +0600

    Added Max product of sum naive

--- 1. First Day Challenge/2. Maximum Pairwise Product.py
+++ 1. First Day Challenge/2. Maximum Pairwise Product.py
@@ -1,6 +1,6 @@
 # Find the maximum product of two distinct numbers in a sequence of non-negative integers.
 
-def max_pair_product(pair):
+def max_pair_product_naive(pair):
     max_product = 0
     for i in range(len(pair)):
         for j in range(i + 1, len(pair)):
@@ -8,9 +8,9 @@ def max_pair_product(pair):
     return max_product
 
 
-n = int(input())
 m = input().split()
 pair = []
 for i in range(len(m)):
     pair.append(int(m[i]))
-print(max_pair_product(pair))
+print(max_pair_product_naive(pair))
+
