b5e0a61ee9c8448a0974bb41202cc767351df522
Debashish Das <talk@ddeb.me>
2020-11-05 19:10:09 +0600

    Initial commit

---
+++ 1. First Day Challenge/1.Sum of Two Integers.py
@@ -0,0 +1,5 @@
+# We will take two intput from user and print sum.
+num_1 = input("Enter The First Number: ")
+num_2 = input("Enter The Second Number: ")
+
+print("The sum of gives numbers is", (int(num_1) + int(num_2)))
